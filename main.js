3d3fd9a9db721bbc7b0f94517b680be15b98728f
Carlos De La Rosa <delarosacarlos04@users.noreply.github.com>
2018-11-11 12:53:37 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-/** Cryptococcus; getting a cookie that can be pulled on the game page */
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
     for(var i = 0; i <ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) === ' ') {
+        while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) === 0) {
+        if (c.indexOf(name) == 0) {
             return c.substring(name.length, c.length);
         }
     }
     return "";
 }
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    
+function addPoints(key, qValue){
+    var newPoints = Number(getCookie(key)) + qValue;
+    var x = String(newPoints);
+        document.write(x);
+        document.cookie = key + "=" + x;
+    
 }
